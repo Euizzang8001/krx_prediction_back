f2e891923a73d02be9f6a92bf459f60d30ca0c47
Euizzang8001 <qkrdmlcks55@korea.ac.kr>
2025-07-14 17:18:23 +0900

    [mod] news를 시간에 따라 다른 collection으로 나누지 않고 하나의 collection에 저장 + schema 오류 수정

--- app/schemas/krx.py
+++ app/schemas/krx.py
@@ -38,7 +38,7 @@ class KrxHistory(BaseModel):
 class News(BaseModel):
     title: str
     description: str
-    url: str
+    link: str
     pub_time: str
 
     model_config = {
@@ -47,7 +47,7 @@ class News(BaseModel):
                 {
                     "title": "마그트론, 중소벤처기업부장관 표창…中 희토류 수출 제한으로 주목",
                     "description": "안준범 마그트론 대표는 “자석은 전기차, <b>반도체</b>, 로봇 등 전략 산업의 핵심 기반 소재이며, 공급망 다변화와 자립…",
-                    "url": "https://n.news.naver.com/mnews/article/018/0006055862?sid=105",
+                    "link": "https://n.news.naver.com/mnews/article/018/0006055862?sid=105",
                     "pub_time": "2025-07-03 16:56:00"
                 }
             ]
